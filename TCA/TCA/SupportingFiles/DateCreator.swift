0bc61dbc1cd30336f296a45facfa2034a43af90e
havrdova <havrdova1lucie@gmail.com>
2022-09-07 10:12:54 +0200

    FEAT: date creator func

---
+++ TCA/TCA/SupportingFiles/DateCreator.swift
@@ -0,0 +1,28 @@
+//
+//  DateCreator.swift
+//  TCA
+//
+//  Created by Lucie Havrdová on 06.09.2022.
+//
+
+import Foundation
+
+func createDate(
+    _ date: String,
+    stringFormat: String
+) -> Date {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = stringFormat
+
+    guard let createdDate = dateFormatter.date(from: date) else {
+        fatalError("Cannot convert date")
+    }
+
+    return createdDate
+}
+
+func dateToString(_ date: Date) -> String {
+    let dateFormater = DateFormatter()
+    dateFormater.dateFormat = "d.M.yyyy"
+    return dateFormater.string(from: date)
+}
