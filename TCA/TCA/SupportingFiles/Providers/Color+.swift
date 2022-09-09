e09f7225710d3135eee3233336f509f3e1ea0853
havrdova <havrdova1lucie@gmail.com>
2022-09-09 14:32:13 +0200

    FEAT: color provider

---
+++ TCA/TCA/SupportingFiles/Providers/Color+.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  TCA
+//
+//  Created by Lucie Havrdová on 09.09.2022.
+//
+
+import SwiftUI
+
+extension Color {
+    static let appPink = Color("")
+    static let appGray = Color("")
+    static let appBackgroundGray = Color("")
+}
